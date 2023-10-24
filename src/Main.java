395bbd4eee18c62c1e8c83762d8e75652faaa89d
rado0999 <radoxml@gmail.com>
2023-10-24 09:50:20 +0700

    add main folder

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.sql.*;
+public class Main {
+	
+	private static final String DB_NAME = "jdbc:mysql://localhost/bams";
+	private static final String DB_USER = "root";
+	
+	private static final String DB_PASSWORD = "Rado@2019";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
