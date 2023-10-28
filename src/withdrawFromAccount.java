695bff75e49dab10896abbe2231e671483a5891e
rado0999 <radoxml@gmail.com>
2023-10-28 13:20:31 +0700

    withraw logic fixed

--- src/withdrawFromAccount.java
+++ src/withdrawFromAccount.java
@@ -39,51 +39,61 @@ public class withdrawFromAccount {
                     String updateSql = "UPDATE accounts SET balance = ? WHERE account_number = ?";
                     //create withraw amount
                     try (PreparedStatement updateStatement = connection.prepareStatement(updateSql)) {
-                    	if(withrawAmount<=currentBalance) {
-                    		updateStatement.setDouble(1, currentBalance - withrawAmount);
-                            
-                    	}else {
+                    	//check user balance that can withraw
+                    	if(withrawAmount < 0) {
                     		updateStatement.setDouble(1, currentBalance);
+                    			
+                    	} else if(withrawAmount >=currentBalance) {
+                    		
+                    		updateStatement.setDouble(1, currentBalance);
+                    	}
+                    	
+                    	else {
+                    		updateStatement.setDouble(1, currentBalance - withrawAmount);
                     	}
                     	
                         updateStatement.setInt(2, accountNumber);
                         int rowsUpdated = updateStatement.executeUpdate();
-                        if(withrawAmount<=currentBalance) {
+                        if( withrawAmount < 0) {
+                        	System.out.print("withdraw failed. Withdraw Amount sould be Greater or Equal to $1.");
+                        	
+                        } else if(withrawAmount>= currentBalance) {
+                        	
+                        	System.out.print("Seem, You don't have enough balance to withraw at this time.");	
+                        	
+                        } else {
+                        	
+                        	
                         	if (rowsUpdated > 0) {
                         		
-                        		  String transactionType = "Withraw";
-                        		  LocalDateTime create_date = LocalDateTime.now();
-                        		    int transactionID = RandomTransactionNum();
-                        		   Timestamp transaction_date = Timestamp.valueOf(create_date);
-                        		   
-                        		   
-                                String insertSqlAmount = "INSERT transactions (transaction_id, account_number, transaction_type, amount, transaction_date) VALUES (?,?,?,?,?)";
-                                try(PreparedStatement insertSqlAmountWithraw = connection.prepareStatement(insertSqlAmount)) {
-                                	insertSqlAmountWithraw.setInt(1, transactionID);
-                                	insertSqlAmountWithraw.setInt(2, accountNumber);
-                                	insertSqlAmountWithraw.setString(3, transactionType);
-                                	insertSqlAmountWithraw.setDouble(4, withrawAmount);
-                                	insertSqlAmountWithraw.setTimestamp(5, transaction_date);
-                                	int rowsinset = insertSqlAmountWithraw.executeUpdate();
-                                	
-                          	      if (rowsinset > 0) {
-                          	        System.out.println("added new transaction to table");
-                          	      } else{
-                          	        System.out.println("Failed");
-                          	      }
-                                
+                      		  String transactionType = "Withraw";
+                      		  LocalDateTime create_date = LocalDateTime.now();
+                      		    int transactionID = RandomTransactionNum();
+                      		   Timestamp transaction_date = Timestamp.valueOf(create_date);
+                      		   
+                      		   
+                              String insertSqlAmount = "INSERT transactions (transaction_id, account_number, transaction_type, amount, transaction_date) VALUES (?,?,?,?,?)";
+                              try(PreparedStatement insertSqlAmountWithraw = connection.prepareStatement(insertSqlAmount)) {
+                              	insertSqlAmountWithraw.setInt(1, transactionID);
+                              	insertSqlAmountWithraw.setInt(2, accountNumber);
+                              	insertSqlAmountWithraw.setString(3, transactionType);
+                              	insertSqlAmountWithraw.setDouble(4, withrawAmount);
+                              	insertSqlAmountWithraw.setTimestamp(5, transaction_date);
+                              	int rowsinset = insertSqlAmountWithraw.executeUpdate();
+                              	
+                 
 								} catch (Exception e) {
 									// TODO: handle exception
 									e.printStackTrace();
 								}
-                            	System.out.println("Withraw successful.");
-                            
-                        	} else {
-                        		System.out.println("Deposit failed. Please try again.");
-                        		}
+                              
+                          	System.out.println("Withraw successful.");
+                          
+                      	} else {
+                      		System.out.println("Withraw failed. Please try again.");
+                      			}
+                      	
                         	
-                        } else {
-                        	System.out.print("withdraw failed. Withdraw Amount sould be Greater or Equal to $1.");
                         }
 
                         
